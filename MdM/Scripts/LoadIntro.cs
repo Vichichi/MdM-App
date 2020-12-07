8c534515123155d961093ebce8eac3eae4a23dba
Victoria Aichhorn <55532366+Vichichi@users.noreply.github.com>
2020-12-07 17:36:43 +0100

    SwipeDetectionMedaillenhalle

---
+++ MdM/Scripts/LoadIntro.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadIntro : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(loadScene(5));
+    }
+
+    IEnumerator loadScene(float WaitSecs)
+    {
+        yield return new WaitForSeconds(WaitSecs);
+        Application.LoadLevel(1);
+    }
+}
